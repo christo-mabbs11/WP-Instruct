4b28442b055588901042d0fc845c7e4846c48832
Christo Mabbs <christo.mabbs@gmail.com>
2019-07-10 19:30:31 +0930

    setup basic plugin functionality

--- wp-instruct.php
+++ wp-instruct.php
@@ -2,8 +2,13 @@
 /**
 * Plugin Name: WP Instruct
 * Plugin URI: https://www.yourwebsiteurl.com/
-* Description: xxxxxxx
+* Description: Create easy to follow instructions for your users in the back-end.
 * Version: 1.0
-* Author: Chirstopher Mabbs
-* Author URI: http://yourwebsiteurl.com/
+* Author: Chirstop Mabbs
+* Author URI: https://github.com/christo-mabbs11
 **/
+
+define( 'INST__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+
+// Add in pliugin main functionality
+require_once( INST__PLUGIN_DIR . 'inc/instructions.php' );
