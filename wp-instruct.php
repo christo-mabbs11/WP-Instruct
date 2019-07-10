861220fe9b9b165723718408410feaaae33d9a66
Christo Mabbs <christo.mabbs@gmail.com>
2019-07-10 19:22:12 +0930

    created empty plugin

---
+++ wp-instruct.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: WP Instruct
+* Plugin URI: https://www.yourwebsiteurl.com/
+* Description: xxxxxxx
+* Version: 1.0
+* Author: Chirstopher Mabbs
+* Author URI: http://yourwebsiteurl.com/
+**/
