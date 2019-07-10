225e23eb139aa8182b7d9be92fb3bc8a686b2a03
Christo Mabbs <christo.mabbs@gmail.com>
2019-07-10 22:38:45 +0930

    add in jquery steps

--- wp-instruct.php
+++ wp-instruct.php
@@ -15,3 +15,11 @@ require_once( INST__PLUGIN_DIR . 'meta-box/meta-box-class/my-meta-box-class.php'
 
 // Add in pliugin main functionality
 require_once( INST__PLUGIN_DIR . 'inc/instructions.php' );
+
+// Add in the scripts for hopscotch
+function my_enqueue($hook) {
+    wp_enqueue_script('hopscotch-js', plugin_dir_url(__FILE__) . '/hopscotch/hopscotch.min.js');
+    wp_register_style( 'hopscotch_admin_css', plugin_dir_url(__FILE__) . '/hopscotch/hopscotch.min.css', false, '1.0.0' );
+    wp_enqueue_style( 'hopscotch_admin_css' );
+}
+add_action('admin_enqueue_scripts', 'my_enqueue');
