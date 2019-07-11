448b7beb492608b27a169767e4539a73755f6437
Christo Mabbs <christo.mabbs@gmail.com>
2019-07-11 19:14:43 +0930

    basic sas s setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,75 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    postcss = require("gulp-postcss"),
+    autoprefixer = require("autoprefixer"),
+    cssnano = require("cssnano"),
+    sourcemaps = require("gulp-sourcemaps");
 
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-  return gulp.src("scss/styles.scss")
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest("public/css"))
-    .pipe(browserSync.stream());
-});
+var paths = {
+    styles: {
+        // By using styles/**/*.sass we're telling gulp to check all folders for any sass file
+        src: "assets/scss/*.scss",
+        // Compiled files will end up in whichever folder it's found in (partials are not compiled)
+        dest: "assets"
+    }
+ 
+    // Easily add additional paths
+    // ,html: {
+    //  src: '...',
+    //  dest: '...'
+    // }
+};
 
-gulp.task('run', ['sass']);
+function style() {
+    return gulp
+        .src(paths.styles.src)
+        // Initialize sourcemaps before compilation starts
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .on("error", sass.logError)
+        // Use postcss with autoprefixer and compress the compiled file using cssnano
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        // Now add/write the sourcemaps
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(paths.styles.dest));
+}
+
+
+
+
+// A simple task to reload the page
+function reload() {
+    browserSync.reload();
+}
+
+// Add browsersync initialization at the start of the watch task
+function watch() {
+    gulp.watch(paths.styles.src, style);
+}
+ 
+// We don't have to expose the reload function
+// It's currently only useful in other functions
+
+    
+// Don't forget to expose the task!
+exports.watch = watch
+
+// Expose the task by exporting it
+// This allows you to run it from the commandline using
+// $ gulp style
+exports.style = style;
+
+/*
+ * Specify if tasks run in series or parallel using `gulp.series` and `gulp.parallel`
+ */
+var build = gulp.parallel(style, watch);
+ 
+/*
+ * You can still use `gulp.task` to expose tasks
+ */
+//gulp.task('build', build);
+ 
+/*
+ * Define default task that can be called by just running `gulp` from cli
+ */
+gulp.task('default', build);
