e15f77101460318fb7f2a606b56e368f6afb5f59
Christo Mabbs <christo.mabbs@gmail.com>
2019-07-11 21:46:51 +0930

    udpate code so that it is pulled from current page instead of url

--- inc/instructions.php
+++ inc/instructions.php
@@ -67,68 +67,70 @@
 
         if (is_admin()){
         
-        /* 
-         * prefix of meta keys, optional
-         * use underscore (_) at the beginning to make keys hidden, for example $prefix = '_ba_';
-         *  you also can make prefix empty to disable it
-         * 
-         */
-        $prefix = 'ba_';
-        
-        /* 
-         * configure your meta box
-         */
-        $config = array(
-          'id'             => 'instruction_meta_box',          // meta box id, unique per meta box
-          'title'          => 'Page Instructions',          // meta box title
-          'pages'          => array('instruction'),      // post types, accept custom post types as well, default is array('post'); optional
-          'context'        => 'normal',            // where the meta box appear: normal (default), advanced, side; optional
-          'priority'       => 'high',            // order of meta box: high (default), low; optional
-          'fields'         => array(),            // list of meta fields (can be added by field arrays)
-          'local_images'   => false,          // Use local or hosted images (meta box images for add/remove)
-          'use_with_theme' => false          //change path if used with theme set to true, false for a plugin or anything else for a custom path(default false).
-        );
-        
-        /*
-         * Initiate your meta box
-         */
-        $my_meta =  new AT_Meta_Box($config);
-        
-        /*
-         * Add fields to your meta box
-         */
+            /* 
+                * prefix of meta keys, optional
+                * use underscore (_) at the beginning to make keys hidden, for example $prefix = '_ba_';
+                *  you also can make prefix empty to disable it
+                * 
+                */
+            $prefix = 'ba_';
+            
+            /* 
+                * configure your meta box
+                */
+            $config = array(
+                'id'             => 'instruction_meta_box',          // meta box id, unique per meta box
+                'title'          => 'Page Instructions',          // meta box title
+                'pages'          => array('instruction'),      // post types, accept custom post types as well, default is array('post'); optional
+                'context'        => 'normal',            // where the meta box appear: normal (default), advanced, side; optional
+                'priority'       => 'high',            // order of meta box: high (default), low; optional
+                'fields'         => array(),            // list of meta fields (can be added by field arrays)
+                'local_images'   => false,          // Use local or hosted images (meta box images for add/remove)
+                'use_with_theme' => false          //change path if used with theme set to true, false for a plugin or anything else for a custom path(default false).
+            );
+            
+            /*
+                * Initiate your meta box
+                */
+            $my_meta =  new AT_Meta_Box($config);
+            
+            /*
+                * Add fields to your meta box
+                */
 
-        // Create the page selector array
-        global $menu, $submenu, $pagenow;
+            // Create the page selector array
+            global $menu, $submenu;
 
-        // Build out menu of pages
-        $be_pages = [];
-        foreach ( $menu as $page ) {    // Top levels pages
+            // Build out menu of pages
+            $be_pages = [];
+            foreach ( $menu as $page ) {    // Top levels pages
 
-            // Ignore empty page names
-            if ( $page[0] && $page[2] && $page[0] != "" && $page[2] != "" ) {
+                // Ignore empty page names
+                if ( $page[0] && $page[2] && $page[0] != "" && $page[2] != "" ) {
 
-                // Remove content in the span tags
-                $tags = array("span");
-                $temp_page = preg_replace('#<(' . implode( '|', $tags) . ')(?:[^>]+)?>.*?</\1>#s', '', $page[0]);
+                    // Remove content in the span tags
+                    $tags = array("span");
+                    $temp_page = preg_replace('#<(' . implode( '|', $tags) . ')(?:[^>]+)?>.*?</\1>#s', '', $page[0]);
 
-                // Sub level pages
-                $temp_sub = $submenu[$page[2]];
-                foreach ( $temp_sub as $sub_page ) {    // Top levels pages
+                    // Sub level pages
+                    $temp_sub = $submenu[$page[2]];
+                    foreach ( $temp_sub as $sub_page ) {    // Top levels pages
 
-                    // Ignore empty page names
-                    if ( $sub_page[0] && $sub_page[2] && $sub_page[0] != "" && $sub_page[2] != "" ) {
+                        // Ignore empty page names
+                        if ( $sub_page[0] && $sub_page[2] && $sub_page[0] != "" && $sub_page[2] != "" ) {
 
-                        // Remove content in the span tags
-                        $tags = array("span");
-                        $temp = preg_replace('#<(' . implode( '|', $tags) . ')(?:[^>]+)?>.*?</\1>#s', '', $sub_page[0]);
-                        if ( $temp == "Add New" ) {
-                            $temp = "Inner/Add New";
+                            // Remove content in the span tags
+                            $tags = array("span");
+                            $temp = preg_replace('#<(' . implode( '|', $tags) . ')(?:[^>]+)?>.*?</\1>#s', '', $sub_page[0]);
+                            if ( $temp == "Add New" ) {
+                                $temp = "Inner/Add New";
+                            }
+                            $temp = $temp_page . " > " . $temp;
+                            
+                            // Save the value
+                            $be_pages[$sub_page[2]] = $temp;
+
                         }
-                        $temp = $temp_page . " > " . $temp;
-                        
-                        // Save the value
-                        $be_pages[$sub_page[2]] = $temp;
 
                     }
 
@@ -136,46 +138,45 @@
 
             }
 
-        }
+            // Select field
+            $my_meta->addSelect( $prefix.'target_page',
+                
+                $be_pages,
 
-        // Select field
-        $my_meta->addSelect( $prefix.'target_page',
+                array('name'=> 'Instruction Page', 'std'=> array($be_pages[0]))
+
+            );
+
+            /*
+                * To Create a reapeater Block first create an array of fields
+                * use the same functions as above but add true as a last param
+                */
+            $repeater_fields[] = $my_meta->addText($prefix.'re_text_field_id_name',array('name'=> 'Instruction','desc'=> "Give this instruction a title."),true);
+            $repeater_fields[] = $my_meta->addText($prefix.'re_text_field_id_selector',array('name'=> 'CSS element selector'),true);
+            $repeater_fields[] = $my_meta->addTextarea($prefix.'re_textarea_field_id',array('name'=> 'Instruction'),true);
             
-            $be_pages,
+            // Select field for placement
+            $repeater_fields[] = $my_meta->addSelect( $prefix.'re_text_field_id_placement',
+                array('top'=> 'Top', 'bottom'=> 'Bottom', 'right'=> 'Right', 'left'=> 'Left'),
+                array('name'=> 'Placement', 'std'=> "top"),
+                true
+            );
 
-            array('name'=> 'Instruction Page', 'std'=> array($be_pages[0]))
+            /*
+                * Then just add the fields to the repeater block
+                */
+            //repeater block
+            $my_meta->addRepeaterBlock($prefix.'re_',array(
+                'inline'   => true, 
+                'name'     => 'Instructions List',
+                'fields'   => $repeater_fields, 
+                'sortable' => true
+            ));
+            
+            //Finish Meta Box Declaration 
+            $my_meta->Finish();
 
-        );
-
-        /*
-         * To Create a reapeater Block first create an array of fields
-         * use the same functions as above but add true as a last param
-         */
-        $repeater_fields[] = $my_meta->addText($prefix.'re_text_field_id_name',array('name'=> 'Instruction','desc'=> "Give this instruction a title."),true);
-        $repeater_fields[] = $my_meta->addText($prefix.'re_text_field_id_selector',array('name'=> 'CSS element selector'),true);
-        $repeater_fields[] = $my_meta->addTextarea($prefix.'re_textarea_field_id',array('name'=> 'Instruction'),true);
-        
-        // Select field for placement
-        $repeater_fields[] = $my_meta->addSelect( $prefix.'re_text_field_id_placement',
-            array('top'=> 'Top', 'bottom'=> 'Bottom', 'right'=> 'Right', 'left'=> 'Left'),
-            array('name'=> 'Placement', 'std'=> "top"),
-            true
-        );
-
-        /*
-         * Then just add the fields to the repeater block
-         */
-        //repeater block
-        $my_meta->addRepeaterBlock($prefix.'re_',array(
-          'inline'   => true, 
-          'name'     => 'Instructions List',
-          'fields'   => $repeater_fields, 
-          'sortable' => true
-        ));
-        
-        //Finish Meta Box Declaration 
-        $my_meta->Finish();
-      }
+        }
 
     }
     add_action( 'admin_init', 'custom_meta_box_info', 100 );
@@ -186,20 +187,15 @@
 
     function custom_add_inst_onto_page () {
 
-        // Get the page current url
-        $page_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
+        // Get rthe current page we're on
+        global $pagenow;
 
-        // Get the part after wp-admin
-        $page_url_after_wpadmin = explode( "wp-admin/", $page_url )[1];
-        
         // Deternine if this is a post inner page (special circumstance)
         $is_posts_page = false;
         $post_type;
-        if (strpos($page_url_after_wpadmin, 'post.php') !== false) {
+        if ( 'post.php' === $pagenow && isset($_GET['post']) ) {
             $is_posts_page = true;
-            $post_id = explode( "post=", $page_url_after_wpadmin )[1];
-            $post_id = explode( "&", $post_id )[0];
-            $post_type = get_post_type( $post_id );
+            $post_type = get_post_type( $_GET['post'] );
         }
         
         // Loop through all queries on the instructions
@@ -250,7 +246,7 @@
                 } else {
 
                     // If this matches the page
-                    if ( $page_url_after_wpadmin == $temp_post_meta["ba_target_page"][0] ) {
+                    if ( $pagenow == $temp_post_meta["ba_target_page"][0] ) {
 
                         $current_inst = $temp_post_meta["ba_re_"];
                         break;
